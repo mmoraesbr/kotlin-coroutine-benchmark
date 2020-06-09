9e486de9ac390e6c681de78d2379c09276631b04
mapaes <ucreiaPass@123>
2020-06-08 23:18:42 -0300

    Impl

---
+++ coroutineapp/src/main/kotlin/lab/kotlin/coroutine/benchmark/coroutineapp/coroutineapp/service/ExternalService.kt
@@ -0,0 +1,41 @@
+package lab.kotlin.coroutine.benchmark.coroutineapp.coroutineapp.service
+
+import lab.kotlin.coroutine.benchmark.coroutineapp.coroutineapp.domain.ResultA
+import lab.kotlin.coroutine.benchmark.coroutineapp.coroutineapp.domain.ResultB
+import lab.kotlin.coroutine.benchmark.coroutineapp.coroutineapp.domain.ResultC
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+
+
+@Component
+class ExternalService {
+
+    @Value("\${external.service.url}")
+    private lateinit var externalServiceUrl: String
+
+    @Autowired
+    private lateinit var webClientBuilder: WebClient.Builder
+
+    private val webClient by lazy {
+        webClientBuilder.build()
+    }
+
+    suspend fun callA() = webClient
+            .get()
+            .uri(externalServiceUrl)
+            .retrieve().awaitBody<ResultA>()
+
+    suspend fun callB() = webClient
+            .get()
+            .uri(externalServiceUrl)
+            .retrieve().awaitBody<ResultB>()
+
+    suspend fun callC() = webClient
+            .get()
+            .uri(externalServiceUrl)
+            .retrieve().awaitBody<ResultC>()
+
+}
